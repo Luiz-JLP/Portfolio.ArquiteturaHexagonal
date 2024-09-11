1f902b6ebb0bc98cec863675a77ca915af5f7083
Luiz-JLP <80367069+Luiz-JLP@users.noreply.github.com>
2024-09-11 14:18:08 -0300

    Criação da infraestrutura de Email

---
+++ PessoasFisicas/Infrastructure.Email/Operations/FakeEmailService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Ports;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Email.Operations
+{
+    public class FakeEmailService(ILogger<FakeEmailService> logger) : IEmailService
+    {
+        public void EnviarEmail(Domain.Entities.Email email)
+        {
+            var mensagem = $"E-mail Enviado - Rementente: {email.De} - Destinatario: {email.Para} - Assunto: {email.Assunto} - Mensagem: {email.Mensagem}";
+            logger.LogInformation(mensagem);
+        }
+    }
+}
---
+++ PessoasFisicas/Infrastructure.Email/Startup/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Ports;
+using Infrastructure.Email.Operations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Email.Startup
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureEmailInfrastructure(this IServiceCollection services)
+        {
+            services.AddDependencyInjection();
+            return services;
+        }
+
+        private static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+        {
+            services.AddScoped<IEmailService, FakeEmailService>();
+            return services;
+        }
+    }
+}
